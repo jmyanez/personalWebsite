4f44e657eda1fcfe4e952627d47c7d3e4beccf42
Jose <manuel1_soccer@hotmail.com>
2024-03-24 21:04:56 -0600

    vite website

---
+++ my-personal-website/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
